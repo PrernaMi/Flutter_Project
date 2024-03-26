6e34cafa9a8a95b0a45b68b4eff18c4199ccedc8
Prerna <125128710+PrernaMi@users.noreply.github.com>
2024-03-26 15:48:29 +0530

    Add files via upload

---
+++ example.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+void main(){
+  // print("Hello World");
+  // print(4+3);
+  // print("3"+"7");
+  // int f = 567;
+  // int s = 4637;
+  // String str = "Prerna";
+  // dynamic a = 647.7;
+  // dynamic b = "Pre";
+  // str = "$str ypp";
+  // print(str.endsWith('p'));
+  // str += "Mitta";
+  // print(str);
+  // print(f);
+  // print(s);
+  // String st = "Prerna ";
+  // st += "Mittal";
+  // print(st);
+  // var variable = DateTime.now();
+  // print(variable);
+  String? str = stdin.readLineSync();
+  print(str);
+  int? a = int.parse(stdin.readLineSync()!);
+  double? b = double.parse(stdin.readLineSync()!);
+  print(a);
+  print(b);
+}
